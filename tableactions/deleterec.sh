a8e6fefd9eea00d4903c6032c62a98aea4c09157
laila672 <Lailatariq672@gmail.com>
2022-01-06 21:49:55 +0200

    delete record

---
+++ tableactions/deleterec.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+echo "Available tables in $1"
+ls data/$1
+read -p "Enter table Name: " tableName
+
+
+if [ -a ./data/$1/$tableName ]
+    then
+    coloumnsNomber=`awk -F: 'NR==1 {print NF}' ./data/$1/$tableName`
+        for (( i=1; i <= $coloumnsNomber; i++ ))
+        do
+            ## this if condition because cut in case of pk is different
+            if testPK=`grep "%:" ./data/$1/$tableName | cut -d ":" -f$i | grep "%PK%" ` 
+            then
+                coloumnsNames[$i]=`grep "%:" ./data/$1/$tableName | cut -d ":" -f$i | cut -d "%" -f3 `
+                coloumnsTypes[$i]=`grep "%:" ./data/$1/$tableName | cut -d ":" -f$i | cut -d "%" -f4 `
+            else
+                coloumnsNames[$i]=`grep "%:" ./data/$1/$tableName | cut -d ":" -f$i | cut -d "%" -f1 `
+                coloumnsTypes[$i]=`grep "%:" ./data/$1/$tableName | cut -d ":" -f$i | cut -d "%" -f2 `
+            fi
+        done
+    
+        ## read condition   
+        for (( i=1; i <= $coloumnsNomber; i++ ))
+        do
+            echo $i"-" ${coloumnsNames[i]} "("${coloumnsTypes[i]}")"
+        done
+
+        ## check if condition index is a number 
+        read -p "condition on which coloumn number : " conditionIndex 
+         expr $conditionIndex + 1 2> /dev/null >> /dev/null
+        while [[ $? -ne 0 || $conditionIndex -le 0 || $conditionIndex -gt $coloumnsNomber ]]
+        do
+            echo "please enter a valid value"
+            read -p "condition on which coloumn number : " conditionIndex 
+         expr $conditionIndex + 1 2> /dev/null >> /dev/null
+        done 
+
+        ## check data type of condition value
+        read -p "Enter a condtion value of type (${coloumnsTypes[conditionIndex]}) : " conditionValue;
+        if [ ${coloumnsTypes[conditionIndex]} == "int" ]
+            then
+         expr $conditionIndex + 1 2> /dev/null >> /dev/null
+            while [ $? != 0 ]
+            do
+                echo "please enter a valid value"
+                read -p "enter (${coloumnsNames[conditionIndex]}) of type (${coloumnsTypes[conditionIndex]}) : " conditionValue
+                expr $conditionIndex + 1 2> /dev/null >> /dev/null
+            done
+        fi
+
+        ## to delete from table
+        awk -F:  ' $"'$conditionIndex'"!="'$conditionValue'" {for(i=1 ;i<=NF ;i++ ) { if (i==NF) print $i; else printf "%s",$i":"}}' ./data/$1/$tableName > ./.tmp;
+        if [ -a ./.tmp ]
+        then
+            cat ./.tmp > ./data/$1/$tableName;
+            rm ./.tmp;
+            echo "Deleted successfully"
+        fi
+
+else
+    echo "there is no such table"
+fi
